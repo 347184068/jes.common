9290a790d6b0a875a3f53f7d84126ddb71d6b98a
lizhi <824261723@qq.com>
2018-03-27 22:15:59 +0800

    增加controller返回标准格式

---
+++ src/main/java/com/lizhivscaomei/jes/common/entity/Msg.java
@@ -0,0 +1,40 @@
+package com.lizhivscaomei.jes.common.entity;
+
+public class Msg {
+    private int code;
+    private boolean success;
+    private String info;
+    private Object data;
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+}
