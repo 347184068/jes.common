aa4860a8e8185e8cae3da389770f34afb91b4c54
邓立芝 <824261723@qq.com>
2018-04-10 15:07:54 +0800

    添加spinnervo

---
+++ src/main/java/com/lizhivscaomei/jes/common/entity/SpinnerVo.java
@@ -0,0 +1,25 @@
+package com.lizhivscaomei.jes.common.entity;
+
+/**
+ * Created by lizhi on 2018/4/10.
+ */
+public class SpinnerVo {
+    private String id;
+    private String text;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
